ee66c63496145971350bfce23ec91b1e858340c2
Jakub Woszczyna <kubwosz@gmail.com>
2018-04-18 23:27:00 +0200

    Initial commit

---
+++ PGS_Internship_Jakub_Woszczyna/Models/PersonalData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGS_Internship_Jakub_Woszczyna.Models
+{
+    class PersonalData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string HomeAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
